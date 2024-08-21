469dcb9959d40afb79c93d25d42d59e9ffe02ab3
NadiyPro <siroviyn13@gmail.com>
2024-08-21 20:13:20 +0300

    creat

--- src/component/PokemonId.tsx
+++ src/component/PokemonId.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useParams} from "react-router-dom";
+import {useAppDispatch, useAppSelector} from "../redux/store";
+import {pokemonAllActions} from "../redux/slices/pokemonAllSlice";
 
 const PokemonId = () => {
+    const {name} = useParams();
+    const pokemon = useAppSelector(state => state.pokemonAllStore.pokemon);
+    const images = useAppSelector(state => state.pokemonAllStore.images);
+    const ability = useAppSelector(state => state.pokemonAllStore.ability);
+    const dispatch = useAppDispatch();
+
+
+    useEffect(() => {
+        if (name){
+            const imag = dispatch(pokemonAllActions.loadPokemonImage(name));
+            const ability = dispatch(pokemonAllActions.loadAbilities(name));
+        }
+    }, [dispatch]);
+
+
     return (
         <div>
-            PokemonId
+
+            {
+                pokemon.map(value => (
+                <div key={value.name}>
+                    <div>{value.name}</div>
+                    <img src={images[value.name]} alt={value.name}/>
+                </div>
+            ))
+            }
+
+
+            {
+                ability.map(value => (
+                    <div key={value.id}>
+                        {value.id} {value.name}
+                    </div>
+                ))
+            }
         </div>
     );
 };
