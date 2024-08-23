db8f954ccc16e8e8eb9bb920bb13fb4589e547ca
NadiyPro <siroviyn13@gmail.com>
2024-08-23 14:01:50 +0300

    creat

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import PokemonAllComponent from "../component/PokemonAllComponent";
 import PokemonId from "../component/PokemonId";
 import FormPokemon from "../component/FormPokemon";
+import Search from "../component/Search";
 
 export const router = createBrowserRouter(
     [
@@ -15,8 +16,8 @@ export const router = createBrowserRouter(
             children: [
                 {index: true, element: <PokemonAllComponent/>},
                 {path:'/pokemon/:name', element: <PokemonId/>},
-                {path:'/pokemon-form/:name', element: <FormPokemon/>}
-                // {path:'comments', element: <CommentsPage/>},
+                {path:'/pokemon-form/:name', element: <FormPokemon/>},
+                {path:'/search/:name', element: <Search/>}
                 // {path:'users/:id', element: <User/>},
                 // {path:'users/:id/posts', element: <Post/>},
                 // {path:'posts/:id/comments', element: <Comment/>}
