21b183ca92768ca26087e92ab49afb076872218f
NadiyPro <siroviyn13@gmail.com>
2024-08-24 19:13:56 +0300

    creat

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Outlet} from "react-router-dom";
+import {useForm} from "react-hook-form";
 
 const MainLayout = () => {
-
+    const {handleSubmit, register} = useForm();
+    const registerHandle = () => console.log()
 
     return (
         <div>
-            <form >
+            <form onSubmit={handleSubmit(registerHandle)}>
                 <label>
-                    <input type={"text"} name={'search'} placeholder={'Enter the name of the pokemon or species or ability'}
+                    <input type={"text"} {...register('name')} placeholder={'Enter the name of the pokemon or species or ability'}
                               className='input_FormComponent'/>
                 </label>
                 <button>Search</button>
