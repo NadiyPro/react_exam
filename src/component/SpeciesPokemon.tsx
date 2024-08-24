c6d00cac9df6358eac05726ea826bdc9670bccba
NadiyPro <siroviyn13@gmail.com>
2024-08-24 14:16:19 +0300

    creat

--- src/component/SpeciesPokemon.tsx
+++ src/component/SpeciesPokemon.tsx
@@ -7,6 +7,7 @@ import {pokemonAllActions} from "../redux/slices/pokemonAllSlice";
 const SpeciesPokemon = () => {
     const {id} = useParams();
     const evolution = useAppSelector(state => state.pokemonAllStore.evolution);
+    const images = useAppSelector(state => state.pokemonAllStore.images);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
@@ -15,9 +16,21 @@ const SpeciesPokemon = () => {
         }
     }, [dispatch,id]);
 
+    useEffect(() => {
+        evolution.forEach(value => {
+            if (!images[value.species.name]) { /**!images value.name перевіряємо чи завантажено вже зображення для цього покемона, щоб не завантажити дубль малюнку**/
+            dispatch(pokemonAllActions.loadPokemonImage(value.species.name));
+            }
+        });
+    }, [images, dispatch]);
+
     return (
         <div>
-            {evolution.map(value => <div key={value.species.name}>{value.species.name}</div>)}
+            {evolution.map(value =>
+                <div key={value.species.name}>
+                    pokemon name: {value.species.name}
+                    <img src={images[value.species.name]} alt={value.species.name}/>
+                </div>)}
         </div>
     );
 };
